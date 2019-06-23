8290f696870dca487df271ea49300d2365fcf278
Danejerosa <Derosa@everettcc.edu>
2019-06-23 10:02:42 -0700

    Uploading Abstract Classes

---
+++ Desktop/Dane/AbstractClasses/AbstractClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resident Dane = new Resident();
+            NonResident Ling = new NonResident();
+
+
+            double balance = Dane.tuition(3);
+            Console.WriteLine(balance);
+
+            balance = Ling.tuition(3);
+            Console.WriteLine(balance);
+
+
+
+        }
+    }
+}
---
+++ Desktop/Dane/AbstractClasses/AbstractClass/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace AbstractClass
+{
+    public abstract class Student
+    {
+        public Student()
+        {
+        }
+
+        public abstract double tuition(int n);
+        
+    }
+
+
+    public class Resident:Student 
+    {
+        public override double tuition(int n)
+        {
+            double balance = 568 * n;
+            if (n > 2)
+            {
+                balance = balance - (balance * .20);
+            }
+
+            return balance;
+        }
+       
+    }
+
+    public class NonResident:Student
+    {
+        public override double tuition(int n)
+        {
+            double balance = 1563 * n;
+            if(n > 2)
+            {
+                balance = balance - (balance * .20);
+            }
+            return balance;
+        }
+    }
+
+}
