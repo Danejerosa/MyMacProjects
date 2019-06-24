2cc8ce7e7667239b1fc0587eca4c2660b0aa0cc3
Danejerosa <Derosa@everettcc.edu>
2019-06-24 01:17:45 -0700

    Uploading solution

---
+++ Desktop/LCProblems/Palindrome/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = -1234;
+            IsPalindrome(n);
+
+        }
+
+        static bool IsPalindrome(int n)
+        {
+
+            string non = n.ToString();
+            int num = n;
+
+
+            char[] inserter = new char[non.Length];
+            char[] reversed = new char[non.Length];
+            inserter = non.ToCharArray();
+
+            int counter = non.Length - 1;
+            for (int i = 0; i < non.Length; i++)
+            {
+
+                reversed[counter] = inserter[i];
+                counter--;
+            }
+            
+            for (int i = 0; i < non.Length; i++)
+            {
+                if(reversed[i] != inserter[i])
+                {
+                    Console.WriteLine("is false");
+                    return false;
+                }
+            }
+
+            return true;
+            
+        }
+    }
+
+
+}
